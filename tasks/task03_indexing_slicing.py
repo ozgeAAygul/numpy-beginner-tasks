0a0d207029dc972eadf5dca537ae871afd30a54d
Ozge Aygul <ozgeaygul62@gmail.com>
2026-02-03 14:08:06 +0300

    Add Task 03: NumPy indexing, slicing, and boolean masks

--- tasks/task03_indexing_slicing.py
+++ tasks/task03_indexing_slicing.py
@@ -6,6 +6,8 @@ arr = np.array([10, 20, 30, 40, 50, 60])
 print(arr[0])
 # print the last index
 print(arr[-1])
+# print the first three indexes
+print(arr[:3])
 # print the middle indexes
 print(arr[1:-1])
 # print the last three indexes
@@ -23,3 +25,16 @@ print(matrix[:, 1])
 print(matrix[-1,-1])
 # print the first two row & the first two column
 print(matrix[0:2, 0:2])
+
+# Boolean Indexing
+# Print elements greater than 30
+masking = arr > 30
+result = arr[masking]
+print(result)
+# Print even elements
+arr_copy = arr.copy()
+result2 = arr_copy[arr % 2 == 0]
+print(result2)
+# Print indexes which bigger and equal to 20 and smaller and equal to 50
+result3 = arr_copy[(arr >= 20) & (arr <= 50)] # arr_copy[20 <= arr <= 50] yazınca hata alıyorum ayrı ayrı yazmam lazımmış conditionları
+print(result3)
